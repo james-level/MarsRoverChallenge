99ce452119866d6f9a6a00dbe4d39179748d3333
jah1603 <hendersonjamesalexander@gmail.com>
2019-03-25 13:01:06 +0000

    canMove() test written and passing

--- test/java/RoverTest.java
+++ test/java/RoverTest.java
@@ -38,5 +38,19 @@ public class RoverTest {
         Assert.assertEquals("2 4 S", rover.stateCurrentLocation());
     }
 
+    @Test
+    public void canMove() {
+        // Given
+        Plateau plateau = new Plateau(5, 5);
+        Coords startingLocation = new Coords(1, 4);
+        Rover rover = new Rover(plateau, startingLocation, Direction.E);
+
+        // When
+        rover.move();
+
+        // Then
+        Assert.assertEquals("2 4 E", rover.stateCurrentLocation());
+    }
+
 
 }
