5aef0524a87232ca5c82c9dd99d9be3487a8d817
jah1603 <hendersonjamesalexander@gmail.com>
2019-03-25 11:16:38 +0000

    Add boundary validation methods to Coords.java

--- src/main/java/rover/environment/Coords.java
+++ src/main/java/rover/environment/Coords.java
@@ -11,9 +11,37 @@ public class Coords {
 
     }
 
+    //   Adds new coordinates for rover
     public Coords newCoordsFor(final int xCoordsStepValue, final int yCoordsStepValue) {
         return new Coords(this.xCoord + xCoordsStepValue, this.yCoord + yCoordsStepValue);
     }
 
+
+    //   Advises if coordinates is off plateau (i.e out-of-bounds)
+    public boolean isOutOfBounds(final Coords coords ){
+        return isXCoordOutOfBounds(coords.xCoord) && isYCoordOutOfBounds(coords.yCoord)
+    }
+
+    private boolean isXCoordOutOfBounds(int xCoord) {
+        return xCoord >= this.xCoord;
+    }
+
+    private boolean isYCoordOutOfBounds(int yCoord) {
+        return yCoord >= this.yCoord;
+    }
+
+
+    //   Advises if coordinates is on plateau (i.e within-bounds)
+    public boolean isWithinBounds(final Coords coords ){
+        return isXCoordWithinBounds(coords.xCoord) && isYCoordWithinBounds(coords.yCoord)
+    }
+
+    private boolean isXCoordWithinBounds(int xCoord) {
+        return xCoord <= this.xCoord;
+    }
+
+    private boolean isYCoordWithinBounds(int yCoord) {
+        return yCoord <= this.yCoord;
+    }
 
 }
